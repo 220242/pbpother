9e325afe304f6e928807ee9a1486902ac7c23de9
220242 <220242@gmail.com>
2021-03-03 07:59:36 +0200

    Create moonlight.sh

---
+++ moonlight.sh
@@ -0,0 +1,24 @@
+sudo apt-get install libssl-dev libopus-dev libasound2-dev libudev-dev \
+                        libavahi-client-dev libcurl4-openssl-dev libevdev-dev \
+                        mercurial libexpat1-dev cmake libgtk-3-dev uuid-dev libenet-dev \  
+                        vdpau-driver-all libvdpau-dev libegl1-mesa-dev libmali-sunxi-dev libenet-dev pkg-config
+cd ~
+mkdir git
+cd git 
+wget http://ffmpeg.org/releases/ffmpeg-4.3.2.tar.bz2
+tar xjvf ffmpeg-4.3.2.tar.bz2
+cd ffmpeg-4.3.2
+./configure --enable-shared
+make
+sudo make install
+cd ~/git
+git clone https://github.com/irtimmer/moonlight-embedded.git
+cd moonlight-embedded
+git submodule update --init
+mkdir build
+cd build/
+cmake ../
+make
+sudo make install
+echo "now pair with desktop (./moonlight pair 192.168.42.2)"
+echo "and stream (./moonlight stream)"
